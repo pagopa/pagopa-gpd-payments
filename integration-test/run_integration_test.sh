811dcd60a7a388873f26ce3299292f12e4a51895
AngeloCaporaso <angelo.caporaso@pagopa.it>
2022-12-19 15:01:21 +0100

    ci (integration test): subkey fetch automation
    Co-authored-by: Federico Ruzzier <FedericoRuzzier@users.noreply.github.com>
    Co-authored-by: Andrea De Rinaldis <andrea-deri@users.noreply.github.com>

--- integration-test/run_integration_test.sh
+++ integration-test/run_integration_test.sh
@@ -1,11 +1,12 @@
 # example: sh ./run_integration_test.sh <local|dev|uat|prod>
 
+export PAYMENTS_SUBSCRIPTION_KEY=$2
+
 # create containers
 cd ../docker || exit
 sh ./run_docker.sh "$1"
 
 # run integration tests
-export PAYMENTS_SUBSCRIPTION_KEY=$2
 cd ../integration-test/src || exit
 yarn install
 yarn test
