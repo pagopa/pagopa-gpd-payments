e6a46b36d49f44802bdf064cf2dafc34fa905d78
AngeloCaporaso <angelo.caporaso@pagopa.it>
2023-01-04 14:45:57 +0100

    ci (smoke test): fix yarn dependencies Co-authored-by: FedericoRuzzier <FedericoRuzzier@users.noreply.github.com> Co-authored-by: Andrea De Rinaldis <andrea-deri@users.noreply.github.com>

--- integration-test/run_integration_test.sh
+++ integration-test/run_integration_test.sh
@@ -13,6 +13,20 @@ cd ../docker || exit
 sh ./run_docker.sh "$1"
 
 # run integration tests
-cd ../integration-test/src || exit
-yarn install
-yarn test
+cd ../integration-test || exit
+
+docker stop node-container-test && docker rm node-container-test
+docker run -dit --name node-container-test node
+docker cp ./src node-container-test:/integration-test
+docker exec -i node-container-test /bin/bash -c " \
+cd ./integration-test
+export APICONFIG_SUBSCRIPTION_KEY=$2 \
+export GPD_SUBSCRIPTION_KEY=$3 \
+export GPS_SUBSCRIPTION_KEY=$4 \
+export DONATIONS_SUBSCRIPTION_KEY=$5 \
+export REST_PAYMENTS_SUBSCRIPTION_KEY=$7 \
+export SOAP_PAYMENTS_SUBSCRIPTION_KEY=$8 \
+export IUVGENERATOR_SUBSCRIPTION_KEY=$6 && \
+yarn install && \
+yarn test"
+docker stop node-container-test && docker rm node-container-test
