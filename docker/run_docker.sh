78324859b8f072a082189da9cc94a42ca63cdedd
AngeloCaporaso <angelo.caporaso@pagopa.it>
2023-01-05 12:47:45 +0100

    ci (smoke test): port publishing

--- docker/run_docker.sh
+++ docker/run_docker.sh
@@ -39,7 +39,7 @@ export containerRegistry=${containerRegistry}
 export image=${image}
 
 stack_name=$(cd .. && basename "$PWD")
-docker compose -p "${stack_name}" up -d --remove-orphans --force-recreate
+docker compose --service-ports -p "${stack_name}" up -d --remove-orphans --force-recreate
 
 # waiting the containers
 printf 'Waiting for the service'
