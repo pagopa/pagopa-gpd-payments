5839f46975c0001d76bc75714c3bd19cb18413e5
FedericoRuzzier <49512050+FedericoRuzzier@users.noreply.github.com>
2023-03-17 09:43:48 +0100

    PAGOPA-605 removing logs

--- integration-test/src/step_definitions/support/utility/axios_common.js
+++ integration-test/src/step_definitions/support/utility/axios_common.js
@@ -17,7 +17,6 @@ function post(url, body, config) {
             return res;
         })
         .catch(error => {
-            console.log(error);
             return error.response;
         });
 }
